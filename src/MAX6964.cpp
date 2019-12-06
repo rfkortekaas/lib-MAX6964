1fb8d04edd3cc710a991e4115dc9528309c41e99
R.F. Kortekaas <rfkortekaas@users.noreply.github.com>
2019-12-06 09:54:35 +0100

    Implemented new registers

--- src/MAX6964.cpp
+++ src/MAX6964.cpp
@@ -1,27 +1,35 @@
 #include <Arduino.h>
-#include <Wire.h>
+#include <TwoWireDevice.h>
 
 #include "MAX6964.h"
 
 bool MAX6964::begin() {
-    TwoWireDevice::begin();
+    static bool init = TwoWireDevice::begin();
 
     // Test read
     read8();
 
-    return (_wire.lastError == 0);
+    return (init);
+}
+
+void MAX6964::configure() {
+    writereg8(MAX6964_CONFIGURATION_REG, 0b0000000);
 }
 
 void MAX6964::setPin(const uint8_t nr) {
-    uint8_t val = readreg16(MAX6964_PHASE0_O15_O8_REG);
+    uint16_t val = readreg16(MAX6964_PHASE0_O7_O0_REG);
     val |= (1<<nr);
 
-    writereg16(MAX6964_PHASE0_O15_O8_REG, val);
+    writereg16(MAX6964_PHASE0_O7_O0_REG, val);
 }
 
 void MAX6964::resetPin(const uint8_t nr) {
-    uint8_t val = readreg16(MAX6964_PHASE0_O15_O8_REG);
+    uint16_t val = readreg16(MAX6964_PHASE0_O7_O0_REG);
     val &= ~(1<<nr);
 
-    writereg16(MAX6964_PHASE0_O15_O8_REG, val);
+    writereg16(MAX6964_PHASE0_O7_O0_REG, val);
+}
+
+void MAX6964::setIntensity(const uint8_t intensity) {
+    writereg8(MAX6964_MASTER_O16_INTENSITY_REG, intensity);
 }
--- src/MAX6964.h
+++ src/MAX6964.h
@@ -4,13 +4,24 @@
 #include <Arduino.h>
 #include <TwoWireDevice.h>
 
-#define MAX6964_ADDRESS_DEFAULT     (0x20)
+#define MAX6964_ADDRESS_DEFAULT             (0x20)
 
-#define MAX6964_PHASE0_O7_O0_REG    (0x02)
-#define MAX6964_PHASE0_O15_O8_REG   (0x03)
-
-#define MAX6964_PHASE1_O7_O0_REG    (0x0A)
-#define MAX6964_PHASE1_O15_O8_REG   (0x0B)
+#define MAX6964_PHASE0_O7_O0_REG            (0x02)
+#define MAX6964_PHASE0_O15_O8_REG           (0x03)
+#define MAX6964_USER_RAM0_REG               (0x06)
+#define MAX6964_USER_RAM1_REG               (0x07)
+#define MAX6964_PHASE1_O7_O0_REG            (0x0A)
+#define MAX6964_PHASE1_O15_O8_REG           (0x0B)
+#define MAX6964_MASTER_O16_INTENSITY_REG    (0x0E)
+#define MAX6964_CONFIGURATION_REG           (0x0F)
+#define MAX6964_O1_O0_INTENSITY_REG         (0x10)
+#define MAX6964_O3_O2_INTENSITY_REG         (0x11)
+#define MAX6964_O5_O4_INTENSITY_REG         (0x12)
+#define MAX6964_O7_O6_INTENSITY_REG         (0x13)
+#define MAX6964_O9_O8_INTENSITY_REG         (0x14)
+#define MAX6964_O11_O10_INTENSITY_REG       (0x15)
+#define MAX6964_O13_O12_INTENSITY_REG       (0x16)
+#define MAX6964_O15_O14_INTENSITY_REG       (0x17)
 
 class MAX6964: public TwoWireDevice {
     public:
@@ -18,14 +29,16 @@ class MAX6964: public TwoWireDevice {
         MAX6964(const uint8_t addr = MAX6964_ADDRESS_DEFAULT) : TwoWireDevice(addr) {};
 
         bool begin();
+        void configure();
         void setPin(const uint8_t nr);
         void resetPin(const uint8_t nr);
+        void setIntensity(const uint8_t intensity);
     
     protected:
 
     private:
         MAX6964(const MAX6964&);
-         MAX6964& operator=(const MAX6964&);
+        MAX6964& operator=(const MAX6964&);
 };
 
 #endif // __MAX6964_h
